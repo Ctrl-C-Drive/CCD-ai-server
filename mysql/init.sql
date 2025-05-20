a3f82a1d0ef02c6cd5821f938e6ad46e65eb0355
rayeonghur <140002805+rayeonghur@users.noreply.github.com>
2025-05-20 15:06:19 +0900

    [FEAT]fastapi 서버 통신 추가

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,4 +1,4 @@
 -- 환경 변수 대신 실제 값 사용 (Docker가 자동으로 치환하지 않음)
-CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'your_password';
+CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON clipboard_db.* TO 'app_user'@'%';
 FLUSH PRIVILEGES;
